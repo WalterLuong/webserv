0b6c1d594f67a3e8f67000883d31e6488bf82589
viporten <viporten@student42.fr>
2022-05-22 22:53:32 +0200

    parsing structure first step

---
+++ Project/parsing/parsing_conffile.hpp
@@ -0,0 +1,66 @@
+#ifndef PARSING_CONFFILE_HPP
+#define PARSING_CONFFILE_HPP
+
+#include <vector>
+#include <string>
+
+
+/* everything is string to helps concatenation for us */
+/* if a variable can be variables, then us vector */
+
+class location_block {
+    std::string     path;
+    std::string     root;
+    std::string     index;
+    std::string     autoindex;
+    int             max_client
+    std::vector<std::string>             allow_methods;
+    std::pair<std::sting, std::string>   error_page;
+    std::pair<std::sting, std::string>   redirection;
+    // need getter to, same as server_block
+}
+
+
+class   server_block {
+    public :
+    std::string     server_name;
+    std::string     root;
+    std::string     index;
+    std::string     autoindex;
+    std::string     port;
+    int             max_client;
+    std::pair<std::sting, std::string>   error_page;
+    std::pair<std::sting, std::string>   redirection;
+    std::vector<std::string>    allow_methods;
+    std::vector<location_block> location;
+
+    // need getter
+    // using string.size() to determine if string is empty can be usefull ex: error_page
+    // 
+    int nbr_location() const { return location.size(); }
+};
+
+class   server_conf {
+
+    public : 
+    std::vector<server_block> server;
+
+    // all getter is to use not for changing the variables (return const)
+    // depending of needed,
+    // server and location are vector, we can use index to reach directly to the target we need
+    // need to find a solution for 0 of location, thinking about use abstract number like -int
+    // need getter to determine index of target
+    bool    is_autoindex_on(int server_index, int location_index) const;
+    const std::vector<std::string>    get_allow_methods(int server_index, int location_index) const;
+    const std::string   get_root(int server_index, int location_index) const 
+    const std::string   get_index(int server_index, int location_index) const;
+    const std::string   get_server_name(int server_index) const;
+    int                 get_max_clien(int server_index, int location_index) const;
+    const std::pair<std::string, std::string> get_error_page(int server_index, int location_index) const;
+    const std::pair<std::string, std::string> get_redirection(int server_index, int location_index) const;
+
+
+
+};
+
+#endif
