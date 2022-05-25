aef02be9164cebf70a0a1750488d5b9cd671dd5d
Victor PORTENSEIGNE <viporten@e2r1p9.clusters.42paris.fr>
2022-05-25 05:27:55 +0200

    create dir for redirection file, error_page to

--- Project/srcs/parsing.cpp
+++ Project/srcs/parsing.cpp
@@ -1,10 +1,13 @@
 #include "../include/webserve.hpp"
 
+int	default_conf(server_conf *stc) {
+
+}
 
 int	parsing(server_conf *stc, int ac, char **av) {
 	if (ac == 1)
 		return (default_conf(stc));
 	if (ac == 2)
-		return (read_and_fill_stc(stc, av[1]));
+		return (read_conffile_fill_stc(stc, av[1]));
 	return (1);
 }
