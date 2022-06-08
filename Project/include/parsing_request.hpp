ee9d5c9f770574cf9eb39a3f098195de0a1d4b53
viporten <viporten@student42.fr>
2022-06-08 03:29:05 +0200

    walter tu casses les couilles

---
+++ Project/include/parsing_request.hpp
@@ -0,0 +1,15 @@
+#ifndef PARSING_REQUEST_HPP
+# define PARSING_REQUEST_HPP
+
+
+class request {
+
+
+	private :
+
+		std::map<std::string, std::string> init_default_error;
+		std::map<std::string, std::string> init_file_type;
+};
+
+
+#endif
