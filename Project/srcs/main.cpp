09bf6b4973d26bfb8d2e64ce8126692b0cbb585f
Victor PORTENSEIGNE <viporten@e2r1p10.clusters.42paris.fr>
2022-06-09 03:04:18 +0200

    add default file issue

--- Project/srcs/main.cpp
+++ Project/srcs/main.cpp
@@ -118,7 +118,7 @@ void	print_stc(server_conf serv) {
 			std::vector<std::pair<int, std::string> >::iterator ip_it;
 			ip_it = it->port_ip.begin();
 			while (ip_it != it->port_ip.end()) {
-//				std::cout << "	" << ip_it->first << " " << ip_it->second << "|" << std::endl; 
+				std::cout << "	" << ip_it->first << " " << ip_it->second << "|" << std::endl; 
 				ip_it++;
 			}
 		}
@@ -185,7 +185,7 @@ int	main(int ac, char **av)
 	std::cout << std::endl;
 	std::cout << std::endl;
 	std::cout << std::endl;
-//	print_stc(serv_conf);
+	print_stc(serv_conf);
 
 
 /*
--- Project/srcs/parsing.cpp
+++ Project/srcs/parsing.cpp
@@ -3,13 +3,9 @@
 #include <string>
 
 
+int	default_conf(server_conf *stc);
 
-int	default_conf(server_conf *stc) {
-	/*implement a default config file with open*/
-	if (stc)
-		return (1);
-	return (1);
-}
+
 
 
 int	read_conffile_fill_stc(server_conf *stc, char *file_name){
@@ -39,6 +35,48 @@ int	read_conffile_fill_stc(server_conf *stc, char *file_name){
 		}
 	}
 	return (0);
+}
+
+int	read_conffile_fill_stc(server_conf *stc,const  char *file_name){
+	std::ifstream file(file_name);
+	std::string line;
+
+	size_t	index;
+
+	if (!file) {
+		return (default_conf(stc));
+	}
+
+	while (getline(file, line)) {
+		/*first for empty line*/
+		server_block	serv_to_fill;
+		if ((line.size() != 0) && (skip_white_space(line) != line.size())) {
+			index = line.find("server");
+			if (index == std::string::npos || check_declaration_server_line(line)) {
+				std::cout << "first you should have a server" << std::endl;
+				return (1);
+			}
+			if (fill_serv(&serv_to_fill, &file, line) != 0) {
+				std::cout << "bad config_file" << std::endl;
+				return (1);
+			}
+			stc->server.push_back(serv_to_fill);
+		}
+	}
+	return (0);
+}
+
+int	default_conf(server_conf *stc) {
+	/*implement a default config file with open*/
+
+	std::ifstream file("../config_file/default.conf");
+	std::string to_read("../config_file/default.conf");
+
+	if (!file) {
+		std::cout << "default file bad :(" << std::endl;
+		return (1);
+	}
+	return (read_conffile_fill_stc(stc, to_read.c_str()));
 }
 
 int	parsing(server_conf *stc, int ac, char **av) {
