fd9ae4821db52b619ec2dca1865bc54a9c39b89b
Victor PORTENSEIGNE <viporten@e2r1p13.42.fr>
2022-06-10 04:47:17 +0200

    add test

--- Project/include/parsing_request.hpp
+++ Project/include/parsing_request.hpp
@@ -4,11 +4,23 @@
 
 class request {
 
+	public :
+		
+		std::string	methods;
+		std::string	http_version;
+		std::string	request;
+		int		chunked;
+		int		validity; //code status pour la map error;
+
+		std::map<std::string, std::string>	map_error;
+		std::map<std::string, std::string>	map_file_type;
+		std::map<std::string, std::string>	instruction;
 
 	private :
 
-		std::map<std::string, std::string> init_default_error;
-		std::map<std::string, std::string> init_file_type;
+		void	init_default_error();
+		void	init_file_type();
+		void	init_instruction();
 };
 
 
