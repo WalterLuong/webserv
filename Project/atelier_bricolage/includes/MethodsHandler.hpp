9f8fe542d071192281140110824a229a46a16b9e
jdidier <jdidier@student.42.fr>
2022-06-14 21:36:31 +0200

    add make_pair() method to MethodsHandler class and everything usefull to use it

--- Project/atelier_bricolage/includes/MethodsHandler.hpp
+++ Project/atelier_bricolage/includes/MethodsHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: jdidier <jdidier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 23:42:19 by jdidier           #+#    #+#             */
-/*   Updated: 2022/06/14 02:41:10 by jdidier          ###   ########.fr       */
+/*   Updated: 2022/06/14 21:12:34 by jdidier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 # include <string>
 # include <map>
 # include <iostream>
-#include "../includes/parsing_conffile.hpp"
+# include "../includes/parsing_conffile.hpp"
+# include "../includes/parsing_request.hpp"
 
 class MethodsHandler {
 	public:
@@ -24,18 +25,27 @@ class MethodsHandler {
 		typedef	body (*exec_method)(void);
 	private:
 		server_conf						_sconf;
-		std::map<std::string, exec_method>		_methods;
+		std::map<std::string, std::string (MethodsHandler::*)()>	_methods;
+		
+
+		std::string		make_header(int const& code);
+
 	public:
-		static std::map<std::string, bool>	METHODS_LIST;
+		static std::map<std::string, bool>			METHODS_LIST;
+		static std::map<std::string, std::string>	HTTP_CODE_LIST;
+		static std::map<std::string, std::string>	FILE_TYPE_LIST;
 		MethodsHandler();
 		MethodsHandler(MethodsHandler const& src);
 		~MethodsHandler();
 		MethodsHandler	&operator=(MethodsHandler const& rhs);
 
-		body	execute(void);
-		body	exec_get(void);
-		body	exec_post(void);
-		body	exec_delete(void);
+		std::string	execute(void);
+		std::string	exec_get(void);
+		std::string	exec_post(void);
+		std::string	exec_delete(void);
 };
+
+std::map<std::string, std::string> init_default_error();
+std::map<std::string, std::string> init_file_type();
 
 #endif
