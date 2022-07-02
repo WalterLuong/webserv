d1142fdcd4494c10c96a45f7e6ef62e72b152d59
WalterLuong <wluong@student.42.fr>
2022-07-02 04:57:56 +0200

    +clean

--- Project/atelier_bricolage/bin/srcs/ResponseHeader.d
+++
@@ -1,6 +0,0 @@
-bin/srcs/ResponseHeader.o: srcs/ResponseHeader.cpp \
-  srcs/../includes/ResponseHeader.hpp srcs/../includes/define.hpp \
-  srcs/../includes/utils.hpp srcs/../includes/../includes/webserve.hpp \
-  srcs/../includes/parsing_conffile.hpp \
-  srcs/../includes/parsing_request.hpp srcs/../includes/webserve.hpp \
-  srcs/../includes/Server.hpp srcs/../includes/Socket.hpp
