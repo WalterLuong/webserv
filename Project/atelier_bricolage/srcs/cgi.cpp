2cfda11e2f49c98526293638188e28ffa967a574
rmechety <rmechety@PC.home>
2022-07-05 16:49:29 +0200

    refacto cgi

--- Project/atelier_bricolage/srcs/cgi.cpp
+++ Project/atelier_bricolage/srcs/cgi.cpp
@@ -1,14 +1,17 @@
 #include "../includes/Response.hpp"
 #include "../includes/utils.hpp"
 
+// clang-format off
+typedef std::vector<std::pair<std::string, std::string> > env_t;
+// clang-format on
 
-
-
-std::string randomdigits(int nb){
+std::string randomdigits(int nb)
+{
 
 	std::stringstream ss;
-	for(int i = 0; i < nb; i++){
-		ss << rand()%10;
+	for (int i = 0; i < nb; i++)
+	{
+		ss << rand() % 10;
 	}
 	return ss.str();
 }
@@ -24,8 +27,6 @@ std::string create_tmpfile(void)
 	ftocreate.close();
 	return std::string(tmpfname);
 }
-
-
 
 std::string joinstr(std::string *strs, int n)
 {
@@ -45,11 +46,9 @@ int run_bin(std::string bin, std::string infilename, std::string outfilename)
 
 	std::string cmd = joinstr(payload, size);
 
-
 	std::cout << "go run cgiL :" << cmd.c_str() << std::endl;
 	return system(cmd.c_str());
 }
-
 
 int write_infile(std::string infile, std::string body)
 {
@@ -100,79 +99,86 @@ std::string get_sub_str(std::string str, size_t pos)
 	return str.substr((pos != std::string::npos) ? pos : 0);
 }
 
-void set_env(request &req, std::string &absolutepath)
+std::vector<std::string> init_env(request &req, std::string &absolutepath)
 {
-
-	/* putenv prend des string static attention */ 
-	putenv((char *)("CONTENT_LENGTH=" + req.instruction["Content-Length"]).c_str());
-	putenv((char *)("CONTENT_TYPE=" + req.instruction["Content-Type"]).c_str());
-
-	putenv((char *)("GATEWAY_INTERFACE=CGI/1.1"));
-
-	// static std::string pathinf = get_sub_str(req.path, req.path.find_last_of("/"));
-	putenv((char *)("PATH_INFO=" + req.path).c_str());
-	putenv((char *)("PATH_TRANSLATED=" + req.path).c_str());
-
-	static std::string query ;
+	static std::string query;
 	query = (req.methods == "GET") ? req.body : "";
-	putenv((char *)("QUERY_STRING=" + query).c_str());
-
-	putenv((char *)("REDIRECT_STATUS=200"));
-
-	putenv((char *)("REQUEST_METHOD=" + req.methods).c_str());
-	putenv((char *)("REQUEST_URI=" + req.path).c_str());
-
 
 	static std::string host =
 		req.instruction["Host"].substr(0, req.instruction["Host"].find_first_of(":"));
 	static std::string port =
 		get_sub_str(req.instruction["Host"], req.instruction["Host"].find_first_of(":") + 1);
 
-	putenv((char *)("SERVER_NAME=" + host).c_str());
-	putenv((char *)("SERVER_PORT=" + port).c_str());
+	char *realp = realpath(absolutepath.c_str(), NULL);
+	if (!realp)
+	{
+		std::cerr << "Realpath error" << std::endl;
+		realp = (char *)"";
+	}
 
-	putenv((char *)("SERVER_PROTOCOL=HTTP/1.1"));
-	putenv((char *)("SERVER_SOFTWARE=Weebserv/1.0"));
+	std::string envs[] = {"CONTENT_LENGTH=" + req.instruction["Content-Length"],
+						  "CONTENT_TYPE=" + req.instruction["Content-Type"],
+						  "GATEWAY_INTERFACE=CGI/1.1",
+						  "PATH_INFO=" + req.path,
+						  "PATH_TRANSLATED=" + req.path,
+						  "QUERY_STRING=" + query,
+						  "REDIRECT_STATUS=200",
+						  "REQUEST_METHOD=" + req.methods,
+						  "REQUEST_URI=" + req.path,
+						  "SERVER_NAME=" + host,
+						  "SERVER_PORT=" + port,
+						  "SERVER_PROTOCOL=HTTP/1.1",
+						  "SERVER_SOFTWARE=Weebserv/1.0",
+						  "REMOTE_IDENT=" + req.instruction["Authorization"],
+						  "REMOTE_USER=" + req.instruction["Authorization"],
+						  "REMOTEaddr=" + port,
+						  "SCRIPT_FILENAME=" + std::string(realp),
+						  "SCRIPT_NAME=" + std::string(realp)};
+	free(realp);
 
-	putenv((char *)("REMOTE_IDENT=" + req.instruction["Authorization"]).c_str());
-	putenv((char *)("REMOTE_USER=" + req.instruction["Authorization"]).c_str());
-	putenv((char *)("REMOTEaddr=" + port).c_str());
+	std::vector<std::string> ret;
+	int size = *(&envs + 1) - envs;
+	for (int i = 0; i < size; i++)
+	{
+		ret.push_back(envs[i]);
+	}
+
 	if (req.instruction["Auth-Scheme"] != "")
-		putenv((char *)("AUTH_TYPE=" + req.instruction["Authorization"]).c_str());
-
-
-	static std::string fname;
-	fname = "SCRIPT_FILENAME="+std::string(realpath(absolutepath.c_str(), NULL));
-	std::cout << "FRAME: " << fname << std::endl;
-	static std::string sname;
-	sname = "SCRIPT_NAME="+std::string(realpath(absolutepath.c_str(), NULL));
-	// static std::string fname = "SCRIPT_FILENAME="+ std::string(getenv("PWD")) + "/"+ absolutepath;
-	putenv((char *)fname.c_str());
-	putenv((char *)sname.c_str());
-
+		ret.push_back("AUTH_TYPE=" + req.instruction["Authorization"]);
+	return ret;
 };
 
-std::string		cgi_handler(request &req, std::string path_for_access, int extension_pos) {
+void set_env(std::vector<std::string> &envs)
+{
+	for (size_t i = 0; i < envs.size(); i++)
+	{
+		putenv((char *)envs[i].c_str());
+	}
+}
 
+std::string cgi_handler(request &req, std::string path_for_access, int extension_pos)
+{
+	std::cerr << __FUNCTION__ << std::endl;
 	std::string bin = req.location_path.cgi_path[extension_pos].second;
-	if (bin != "") {
-		std::cout << "_RED" << path_for_access << "_NOR" << std::endl;
-		set_env(req, path_for_access);
-	//	path_for_access += ""; 
+	if (bin != "")
+	{
+		std::vector<std::string> envs = init_env(req, path_for_access);
+		set_env(envs);
 		return cgi_execution(bin, req.body);
 	}
 	std::cerr << "CGI not found" << std::endl;
 	return "";
-
-
 }
 
-int			get_cgi_path_pos(std::string extension, std::vector<std::pair<std::string, std::string> > cgi_path)  {
+int get_cgi_path_pos(std::string extension, env_t cgi_path)
+{
 
-	std::vector<std::pair<std::string, std::string> >::iterator res = cgi_path.begin();
+	env_t::iterator res = cgi_path.begin();
 	int i = 0;
-	while (res != cgi_path.end()) {
-		if (res->first == extension) {
+	while (res != cgi_path.end())
+	{
+		if (res->first == extension)
+		{
 			return i;
 		}
 		res++;
